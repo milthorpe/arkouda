f67f730ea949c0ffc9d2a0260738cf1fa72af46a
Josh Milthorpe <josh.milthorpe@gmail.com>
2022-07-27 08:18:46 -0400

    fix sort for multiple types

--- src/cubSort.h
+++ src/cubSort.h
@@ -1,4 +1,7 @@
-#ifndef _COUNT_DIGITS_H_
-#define _COUNT_DIGITS_H_
-void cubSortPairs(double *d_keys_in, double *d_keys_out, int64_t *d_values_in, int64_t *d_values_out, size_t N);
+#ifndef _CUB_SORT_H_
+#define _CUB_SORT_H_
+void cubSortPairs_int32(int32_t *d_keys_in, int32_t *d_keys_out, int64_t *d_values_in, int64_t *d_values_out, size_t N);
+void cubSortPairs_int64(int64_t *d_keys_in, int64_t *d_keys_out, int64_t *d_values_in, int64_t *d_values_out, size_t N);
+void cubSortPairs_float(float *d_keys_in, float *d_keys_out, int64_t *d_values_in, int64_t *d_values_out, size_t N);
+void cubSortPairs_double(double *d_keys_in, double *d_keys_out, int64_t *d_values_in, int64_t *d_values_out, size_t N);
 #endif
