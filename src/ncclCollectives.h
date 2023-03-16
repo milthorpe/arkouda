56344cc1c9b19a2d1b967883843d1ff281d0e587
Josh Milthorpe <josh.milthorpe@gmail.com>
2023-03-15 23:08:43 -0400

    fix function prototype for gpuCommGetUniqueID

--- src/ncclCollectives.h
+++ src/ncclCollectives.h
@@ -1,7 +1,7 @@
 #ifndef _NCCL_COLLECTIVES_H_
 #define _NCCL_COLLECTIVES_H_
 #include <stdint.h>
-void *gpuCommGetUniqueID();
+void *gpuCommGetUniqueID(void);
 void *gpuCommInitRank(int num_ranks, void* comm_id, int rank);
 void gpuCommDestroy(void *comm_ptr);
 void gpuReduce_sum_int32(const void *sendbuff, void *recvbuff, size_t count, int root, void *comm_ptr);
