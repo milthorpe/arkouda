cfb5ef400f5822664689b7c62b47cb9fdb5c772f
Josh Milthorpe <josh.milthorpe@gmail.com>
2023-07-31 08:29:36 -0400

    use HipCUB for sum

--- src/cubSum.cu
+++ src/cubSum.cu
@@ -1,8 +1,10 @@
 #include <hipcub/hipcub.hpp>
 #include <stdio.h>
-#include "cuda_error_check.h"
+#include "hip_error_check.h"
 
-using namespace cub;
+using namespace hipcub;
+
+#define DebugExit(x) if (HipcubDebug(x)) exit(1);
 
 template <typename T> void cubSum(const T *d_in, T *d_out, int64_t num_items) {
   // Determine temporary device storage requirements
@@ -10,17 +12,17 @@ template <typename T> void cubSum(const T *d_in, T *d_out, int64_t num_items) {
   size_t temp_storage_bytes = 0;
   CachingDeviceAllocator  g_allocator(true);  // Caching allocator for device memory
 
-  CubDebugExit(DeviceReduce::Sum(d_temp_storage, temp_storage_bytes, d_in, d_out, num_items));
+  DebugExit(DeviceReduce::Sum(d_temp_storage, temp_storage_bytes, d_in, d_out, num_items));
 
   // Allocate temporary storage
   //CudaSafeCall(cudaMalloc(&d_temp_storage, temp_storage_bytes));
-  CubDebugExit(g_allocator.DeviceAllocate(&d_temp_storage, temp_storage_bytes));
+  DebugExit(g_allocator.DeviceAllocate(&d_temp_storage, temp_storage_bytes));
 
   // Compute Sum
-  CubDebugExit(DeviceReduce::Sum(d_temp_storage, temp_storage_bytes, d_in, d_out, num_items));
+  DebugExit(DeviceReduce::Sum(d_temp_storage, temp_storage_bytes, d_in, d_out, num_items));
 
   //CudaSafeCall(cudaFree(d_temp_storage));
-  if (d_temp_storage) CubDebugExit(g_allocator.DeviceFree(d_temp_storage));
+  if (d_temp_storage) DebugExit(g_allocator.DeviceFree(d_temp_storage));
 }
 
 extern "C" {
